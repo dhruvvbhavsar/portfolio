a8a7bf1d00053cba98c75d4a7a5d6aa55c04501e
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-03-31 14:30:04 +0530

    intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -124,7 +124,7 @@
 				class="z-20 mb-10 h-12 w-12 rounded-full border-2 border-black"
 			/>
 		</div>
-		<div class="sm:col-span-2 col-span-6 grid grid-col-6 h-72 bg-purple-900 rounded-2xl ">
+		<div class="sm:col-span-2 col-span-6 grid grid-col-6 p-2 h-72 bg-purple-900 rounded-2xl ">
 		<div class="w-full grid grid-cols-4 grid-rows-4 py-4 [&>svg]:w-full [&>svg]:text-center">
 			<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>HTML5</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/></svg>
 			<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>CSS3</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"/></svg>
@@ -148,6 +148,10 @@
 			<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>OpenAI</title><path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z"/></svg>
 		</div>
 		</div>
-		<div class="sm:col-span-4 col-span-6 h-72 bg-sky-500 rounded-2xl " />
+		<div class="col-span-6 h-72 rounded-2xl bg-green-400 p-3 text-sm sm:text-lg sm:col-span-4 flex flex-col items-center justify-evenly intro">
+			<p class="font-mono text-2xl sm:text-3xl text-stone-950">More than meets the 🌟</p>
+			<p class="font-medium">Hey there, I'm Dhruv! 🤖 As a Front End Engineer from India, I've been coding since I was a little kid 🔍 trying to unlock cheat codes on GTA San Andreas 🎮 and save Princess Peach 🍑 in Mario.</p>
+			<p class="font-medium">When I'm not 🖥️ programming, you can catch me watching 🍿 movies, jamming to 🎶 music, or getting lost in a good novel 📚. And if you want to chat, let's meet up..I'll be waiting for you on the beach🏖️</p>
+		  </div>
 	</div>
 </div>
