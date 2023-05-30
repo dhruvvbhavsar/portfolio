f359430ca9675bcc028c6052edc4f9a1368c75b3
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-05-30 18:32:59 +0530

    update: projects added

--- src/routes/Stuff/+page.svelte
+++ src/routes/Stuff/+page.svelte
@@ -1,3 +1,141 @@
-<div class="flex flex-col justify-center items-center h-screen">
-    <p class="sm:text-4xl text-lg font-mono font-extrabold">Page under Construction🏗️</p>
-</div>
+<section class="flex h-screen flex-col md:flex-row bg-black">
+	<div
+		class="h-2/5 w-full bg-[url('https://e0.pxfuel.com/wallpapers/157/912/desktop-wallpaper-on-sri-krishna-in-2022-simple-iphone-nature-cute-small-drawings-krishna-black.jpg')] bg-cover bg-center md:h-screen"
+	/>
+
+	<div class="flex h-3/5 flex-col justify-center md:h-screen md:w-4/5">
+		<h1 class="my-3 text-center font-sans text-3xl font-extrabold text-white">Krishna AI</h1>
+		<p class="text-center text-sm text-white">
+			Engage in conversations with Lord Krishna, the Supreme Personality of Godhead, as Arjuna did
+			in the battlefield of Kurukshetra 5000 years ago, and discover profound wisdom and divine
+			guidance. Experience the timeless teachings of Lord Krishna through this AI chatbot, allowing
+			you to delve into spiritual knowledge and gain insights into universal truths.
+		</p>
+		<nav class="flex justify-evenly pt-5">
+			<a href="https://github.com/dhruvvbhavsar/krishna-ai" target="_blank">
+				<svg
+					role="img"
+					width="24"
+					fill="gray"
+					height="24"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<title>GitHub</title>
+					<path
+						d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+					/>
+				</svg>
+			</a>
+			<a href="https://krishna-ai.vercel.app" target="_blank">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					fill="gray"
+					class="bi bi-box-arrow-up-right"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+					/>
+					<path
+						fill-rule="evenodd"
+						d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+					/>
+				</svg>
+			</a>
+		</nav>
+	</div>
+</section>
+
+<section class="flex h-screen flex-col md:flex-row-reverse bg-black">
+	<div
+		class="h-2/5 w-full bg-[url('https://en-media.thebetterindia.com/uploads/2017/04/Legistify-Forced-Marriage.jpg')] bg-cover bg-center md:h-screen"
+	/>
+
+	<div class="flex h-3/5 flex-col justify-center md:h-screen md:w-4/5">
+		<h1 class="my-3 text-center font-sans text-3xl font-extrabold text-white">Shubh Sambandh</h1>
+		<p class="text-center text-sm text-white">
+			Welcome to Shubh Sambandh, the ultimate matrimony web app that connects hearts for a lifetime.
+			With Shubh Sambandh, you can create your profile, explore potential matches, and unlock a
+			plethora of exciting features designed to help you find your perfect life partner. Get ready
+			to embark on a remarkable journey towards everlasting love.
+		</p>
+		<nav class="flex justify-evenly pt-5">
+			<a href="https://github.com/dhruvvbhavsar/Shubh-Sambandh" target="_blank">
+				<svg
+					role="img"
+					width="24"
+					fill="gray"
+					height="24"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<title>GitHub</title>
+					<path
+						d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+					/>
+				</svg>
+			</a>
+			<a href="https://shubhsambandh.com" target="_blank">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					fill="gray"
+					class="bi bi-box-arrow-up-right"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+					/>
+					<path
+						fill-rule="evenodd"
+						d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+					/>
+				</svg>
+			</a>
+		</nav>
+	</div>
+</section>
+
+<section class="flex h-screen flex-col md:flex-row bg-black">
+	<div
+		class="h-2/5 w-full bg-[url('https://prod-images-static.radiopaedia.org/images/1371188/0a1f5edc85aa58d5780928cb39b08659c1fc4d6d7c7dce2f8db1d63c7c737234_gallery.jpeg')] bg-cover bg-center md:h-screen"
+	/>
+
+	<div class="flex h-3/5 flex-col justify-center md:h-screen md:w-4/5">
+		<h1 class="my-3 text-center font-sans text-3xl font-extrabold text-white">
+			Pneumonia Detector
+		</h1>
+		<p class="text-center text-sm text-white">
+			Introducing the Pneumonia Detector, a groundbreaking project powered by Machine Learning and
+			developed using the cutting-edge ResNet18 model. This innovative tool allows you to
+			effortlessly upload your chest X-ray images and receive accurate predictions about the health
+			of your lungs, distinguishing between normal and abnormal conditions with an impressive 92%
+			accuracy rate. Experience the power of AI in healthcare as we strive to detect and combat
+			pneumonia effectively.
+		</p>
+		<nav class="flex justify-evenly pt-5">
+			<a
+				href="https://colab.research.google.com/drive/1oxHJByaaG-SFw48-f50_KzmIeqDRM1ic?usp=sharing"
+				target="_blank"
+			>
+				<svg
+					role="img"
+					viewBox="0 0 24 24"
+					width="24"
+					height="24"
+					fill="gray"
+					xmlns="http://www.w3.org/2000/svg"
+					><title>Google Colab</title><path
+						d="M16.9414 4.9757a7.033 7.033 0 0 0-4.9308 2.0646 7.033 7.033 0 0 0-.1232 9.8068l2.395-2.395a3.6455 3.6455 0 0 1 5.1497-5.1478l2.397-2.3989a7.033 7.033 0 0 0-4.8877-1.9297zM7.07 4.9855a7.033 7.033 0 0 0-4.8878 1.9316l2.3911 2.3911a3.6434 3.6434 0 0 1 5.0227.1271l1.7341-2.9737-.0997-.0802A7.033 7.033 0 0 0 7.07 4.9855zm15.0093 2.1721l-2.3892 2.3911a3.6455 3.6455 0 0 1-5.1497 5.1497l-2.4067 2.4068a7.0362 7.0362 0 0 0 9.9456-9.9476zM1.932 7.1674a7.033 7.033 0 0 0-.002 9.6816l2.397-2.397a3.6434 3.6434 0 0 1-.004-4.8916zm7.664 7.4235c-1.38 1.3816-3.5863 1.411-5.0168.1134l-2.397 2.395c2.4693 2.3328 6.263 2.5753 9.0072.5455l.1368-.1115z"
+					/></svg
+				>
+			</a>
+		</nav>
+	</div>
+</section>
